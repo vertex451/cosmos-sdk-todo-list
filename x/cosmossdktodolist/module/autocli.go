7e0c160d657aa4282c13e63db560dbefc2137586
Artem <vertex451@gmail.com>
2025-07-13 12:21:43 +0200

    ignite scaffold query get-todos-by-status

--- x/cosmossdktodolist/module/autocli.go
+++ x/cosmossdktodolist/module/autocli.go
@@ -31,6 +31,13 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "id"}},
 				},
 
+				{
+					RpcMethod:      "GetTodosByStatus",
+					Use:            "get-todos-by-status [completed]",
+					Short:          "Query get-todos-by-status",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "completed"}},
+				},
+
 				// this line is used by ignite scaffolding # autocli/query
 			},
 		},
